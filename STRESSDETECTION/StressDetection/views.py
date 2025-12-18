974be2af0ce158ef3ae488c7c6ed8629af2e5441
Chandra Sekhara Krishna Akash Nutakki <161455329+cskakashnutakki@users.noreply.github.com>
2025-12-18 16:04:47 +0530

    Update views.py

--- STRESSDETECTION/StressDetection/views.py
+++ STRESSDETECTION/StressDetection/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from users.forms import UserRegistrationForm
+# from django.shortcuts import redirect
 
 
 def index(request):
@@ -15,6 +17,21 @@ def AdminLogin(request):
     return render(request, 'AdminLogin.html', {})
 
 
+# def UserRegister(request):
+#     form = UserRegistrationForm()
+#     return render(request, 'UserRegistrations.html', {'form': form})
+
 def UserRegister(request):
-    form = UserRegistrationForm()
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()   # 🔥 THIS SAVES USER TO DATABASE
+            messages.success(request, "Registration successful. Please login.")
+            return redirect('UserLogin')
+        else:
+            messages.error(request, "Registration failed. Please check details.")
+    else:
+        form = UserRegistrationForm()
+
     return render(request, 'UserRegistrations.html', {'form': form})
+
