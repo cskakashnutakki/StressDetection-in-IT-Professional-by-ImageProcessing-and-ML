97f40bb20ec5d1f92f3ac20b9a9a14892429b31e
Chandra Sekhara Krishna Akash Nutakki <161455329+cskakashnutakki@users.noreply.github.com>
2025-12-18 15:55:01 +0530

    Add static URL configuration to urls.py
    Added static URL configuration for serving static files.

--- STRESSDETECTION/StressDetection/urls.py
+++ STRESSDETECTION/StressDetection/urls.py
@@ -21,6 +21,7 @@ from admins import views as admins
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -55,3 +56,4 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
